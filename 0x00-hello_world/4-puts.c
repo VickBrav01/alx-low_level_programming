ae2fe7ea0551733476b855d166dddbe9dca3b629
VickBrav01 <victortosh001@gmail.com>
2023-07-13 15:00:39 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,7 @@
 #include <stdio.h>
 /**
-  * int main(void) - Prints Programming is like building a multilingual puzzle
-  * Use the function puts
-  * You are not allowed to use printf
-  * Return:  0
+  * main - Prints Programming is like building a multilingual puzzle
+  * Return: 0
   */
 
 int main(void)
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+  * main - A programs that prints using printf
+  * Return: 0
+  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
